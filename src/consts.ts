435926a6701cb45a2c23f8d034a38efa423a0d4c
Aziel Choi <aziel@yousub.io>
2026-01-29 16:00:15 +0900

    Update site configuration and footer content; enhance about page details

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "doerchoi's 지식저장소";
+export const SITE_DESCRIPTION = "배운 지식들을 저장하여 습득하고 나눕니다.";
